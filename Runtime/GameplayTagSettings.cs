9decac40dab007d856f35fb7ae8dd7f8a0b71b4b
luuuuyang <luuuuyang@163.com>
2025-03-22 16:13:16 +0800

    fix build error

--- Runtime/GameplayTagSettings.cs
+++ Runtime/GameplayTagSettings.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace GameplayTags
 {
     [CreateAssetMenu(fileName = "GameplayTagSettings", menuName = "GameplayAbilities/GameplayTagSettings")]
     public class GameplayTagSettings : GameplayTagsList
-    {   
+    {
         [ReadOnly]
         public bool ImportTagsFromConfig = true;
 
@@ -22,23 +25,27 @@ namespace GameplayTags
 
         public static GameplayTagSettings GetOrCreateSettings()
         {
-            MonoScript script = MonoScript.FromScriptableObject(CreateInstance<GameplayTagSettings>());
-            string scriptPath = AssetDatabase.GetAssetPath(script);
-            string settingsPath = scriptPath.Replace("Runtime/GameplayTagSettings.cs", "Editor/Config/" + GameplayTagSource.DefaultName);
+            // MonoScript script = MonoScript.FromScriptableObject(CreateInstance<GameplayTagSettings>());
+            // string scriptPath = AssetDatabase.GetAssetPath(script);
+            // string settingsPath = scriptPath.Replace("Runtime/GameplayTagSettings.cs", "Editor/Config/" + GameplayTagSource.DefaultName);
 
-            GameplayTagSettings settings = AssetDatabase.LoadAssetAtPath<GameplayTagSettings>(settingsPath);
-            if (settings == null)
-            {
-                settings = CreateInstance<GameplayTagSettings>();
-                AssetDatabase.CreateAsset(settings, settingsPath);
-                AssetDatabase.SaveAssets();
-            }
+            // GameplayTagSettings settings = AssetDatabase.LoadAssetAtPath<GameplayTagSettings>(settingsPath);
+            // if (settings == null)
+            // {
+            //     settings = CreateInstance<GameplayTagSettings>();
+            //     AssetDatabase.CreateAsset(settings, settingsPath);
+            //     AssetDatabase.SaveAssets();
+            // }
+
+            GameplayTagSettings settings = Resources.Load<GameplayTagSettings>("DefaultGameplayTags");
             return settings;
         }
 
+#if UNITY_EDITOR
         public static SerializedObject GetSerializedObject()
         {
             return new SerializedObject(GetOrCreateSettings());
         }
+#endif
     }
 }
